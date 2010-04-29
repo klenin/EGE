79812ce0dec13bdc35e60e656cb241c989ec2313
Alexander S. Klenin <klenin@gmail.com>
2010-04-30 00:43:12 +1100

    Генерация заданий A3 типа "количество едениц"

--- EGE/Generate.pm
+++ EGE/Generate.pm
@@ -163,4 +163,19 @@ sub A2_units {
     };
 }
 
+sub A3_ones {
+    my $npower = rnd->in_range(5, 9);
+    my $case = rnd->pick(
+        { d => 0, a => 0 }, { d => 1, a => 1 }, { d => -1, a => 2 },
+    );
+    my $n = 2 ** $npower + $case->{d};
+
+    {
+        question => "Сколько единиц в двоичной записи числа $n?",
+        variants => [ 1, 2, $npower - 1, $npower ],
+        answer => $case->{a},
+        variants_order => 'random',
+    };
+}
+
 1;
