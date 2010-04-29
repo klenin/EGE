b2f62223e3d420e637e1b6b4067d2ffc9fba1667
Alexander S. Klenin <klenin@gmail.com>
2010-04-30 03:44:48 +1100

    Генерация заданий A4

---
+++ EGE/Gen/A4.pm
@@ -0,0 +1,25 @@
+package EGE::Gen::A4;
+
+use strict;
+use warnings;
+
+use Bit::Vector;
+use EGE::Random;
+use EGE::Bin;
+
+sub sum {
+    my $av = rnd->in_range(17, 127);
+    my $bv = rnd->in_range(17, 127);
+    my $r = $av + $bv;
+    my ($atext, $btext) = map hex_or_oct($_), $av, $bv;
+    my $q = qq~Чему равна сумма чисел <i>a</i> = $atext и <i>b</i> = $btext?~;
+    my @errors = rnd->pick_n(3, map $av ^ (1 << $_), 0..7);
+    {
+        question => $q,
+        variants => [ map bin_hex_or_oct($_, rnd->in_range(0, 2)), $r, @errors ],
+        answer => 0,
+        variants_order => 'random',
+    };
+}
+
+1;
