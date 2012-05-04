987a24e9407b8d58822d2824d8313726eaa0e096
Alexander S. Klenin <klenin@gmail.com>
2012-05-05 07:34:36 +1100

    Выдавать валидный xhtml в заданиях, добавленных в предыдущем коммите

--- EGE/Gen/B12.pm
+++ EGE/Gen/B12.pm
@@ -18,24 +18,24 @@ use List::Util 'min';
 sub search_query {
     my ($self) = @_;
     my ($item1, $item2) = rnd->pick_n(2, qw(Шахматы Теннис Волейбол Хоккей Футбол
-                                            Плаванье Баскетбол Дартс Гольф Биатлон));
+                                            Плавание Баскетбол Дартс Гольф Биатлон));
     my ($item1_cnt, $item2_cnt) = map { 10 * rnd->in_range(200, 999) } 1, 2;
     my $both_cnt = 10 * rnd->in_range(100, int min($item1_cnt, $item2_cnt)/20);
 
     my @variants = (["$item1 | $item2", $item1_cnt + $item2_cnt - $both_cnt],
                     ["$item1", $item1_cnt],
                     ["$item2", $item2_cnt],
-                    ["$item1 & $item2", $both_cnt]);
+                    ["$item1 &amp; $item2", $both_cnt]);
 
     my $to_find = splice @variants, rnd->in_range(0, 3), 1;
     $self->{correct} = $to_find->[1];
 
-    $_ = html->row('th', '<b>Запрос</b>', '<b>Найдено страниц (в тысячах)');
+    $_ = html->row('th', '<b>Запрос</b>', '<b>Найдено страниц (в тысячах)</b>');
     $_ .= join '', map { html->row('td', @$_) } @variants;
-    my $table = html->table($_, {border => 1, style => 'text-align: center'});
+    my $table = html->table($_, { border => 1, style => 'text-align: center' });
     $self->{text} = <<EOL
 В языке запросов поискового сервера для обозначения логической операции «ИЛИ»
-используется символ «|», а для логической операции «И» – символ «&».
+используется символ «|», а для логической операции «И» – символ «&amp;».
 В таблице приведены запросы и количество найденных по ним страниц некоторого
 сегмента сети Интернет. 
 $table
