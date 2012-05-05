04ad93b4a9f80cc2cb20b624e792de891cd94663
vkevroletin <kevroletin@gmail.com>
2012-05-05 00:58:47 +0000

    Исправление для генератора B05::calculator
    Проблема: иногда выдаёт неоднозначное решение - требуется ввести последовательность
    действий, но бывает, что таких последовательностей несколько.
    Причина: в переборе всех последовательностей решений первая последовательность пропускалась.
    Решение: учитывать пропущенную последовательность.

--- EGE/Gen/B05.pm
+++ EGE/Gen/B05.pm
@@ -72,7 +72,9 @@ sub calculator {
         $arg = rnd->in_range(2, 10);
         $prg = [ (0) x $num ];
         my %results;
-        ++$results{apply($cmd, $prg, $arg)} while next_prg($cmd, $prg);
+        do {
+            ++$results{apply($cmd, $prg, $arg)}
+        } while next_prg($cmd, $prg);
         my @r = grep 50 < $_ && $_ < 1000 && $results{$_} == 1, keys %results;
         $result = rnd->pick(@r) if @r;
     } until $result;
