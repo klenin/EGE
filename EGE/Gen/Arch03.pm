36c450545939365a850694d8c35c4fb74b24ec10
Alexander S. Klenin <klenin@gmail.com>
2013-06-03 21:50:14 +1100

    Переписать задание Arch03
    * Стиль и форматирование кода
    * Использовать in_range вместо pick

--- EGE/Gen/Arch03.pm
+++ EGE/Gen/Arch03.pm
@@ -13,25 +13,36 @@ use EGE::Asm::Processor;
 use EGE::Asm::AsmCodeGenerate;
 
 sub choose_commands_mod_3 {
-	my $self = shift;
-	cgen->{code} = [];
-	my $reg = cgen->get_reg(8);
-	my $jmp = rnd->pick('jnc', 'jns', 'js');
-	my $arg = { jnc => rnd->pick(0..41)*6 + 3, jns => rnd->pick(0..20)*6 + 1, js => rnd->pick(21..41)*6 + 3 }->{$jmp};
-	my $label = 'L';
-	my $val_add = rnd->in_range(1, 3);
-	cgen->add_command('mov', $reg, $arg);
-	cgen->add_command($label.':');
-	cgen->add_command('add', $reg, $val_add);
-	cgen->add_command($jmp, $label);
-	$self->formated_variants('<code>%s</code>', "mov $reg, $arg", "$label:", "add $reg, 1", "add $reg, 2", "$jmp $label");
-	$self->{correct} = {1 => [1, 1, 1, 0, 1], 2 => [1, 1, 0, 1, 1], 3 => [1, 1, 1, 1, 1]}->{$val_add};
-	proc->run_code(cgen->{code});
-	my $res = proc->get_val($reg);
-	$self->{text} = <<QUESTION
-Выделите команды, которые следует оставить в программе, чтобы после выполнения полученного кода в регистре $reg содержалось значение $res
-QUESTION
-;
+    my $self = shift;
+    cgen->{code} = [];
+    my $reg = cgen->get_reg(8);
+    my ($jmp, $arg) = @{rnd->pick(
+        [ jnc => rnd->in_range(0, 41) * 6 + 3 ],
+        [ jns => rnd->in_range(0, 20) * 6 + 1 ],
+        [ js => rnd->in_range(21, 41) * 6 + 3 ],
+    )};
+    my $label = 'L';
+    (my $val_add, $self->{correct}) = @{rnd->pick(
+        [ 1 => [ 1, 1, 1, 0, 1 ] ],
+        [ 2 => [ 1, 1, 0, 1, 1 ] ],
+        [ 3 => [ 1, 1, 1, 1, 1 ] ],
+    )};
+    cgen->add_commands(
+        [ 'mov', $reg, $arg ],
+        [ "$label:" ],
+        [ 'add', $reg, $val_add ],
+        [ $jmp, $label ]);
+    $self->formated_variants('<code>%s</code>',
+        "mov $reg, $arg",
+        "$label:",
+        "add $reg, 1",
+        "add $reg, 2",
+        "$jmp $label");
+    proc->run_code(cgen->{code});
+    my $res = proc->get_val($reg);
+    $self->{text} =
+        'Выделите команды, которые следует оставить в программе, ' .
+        "чтобы после выполнения полученного кода в регистре $reg содержалось значение $res";
 }
 
 1;
