459fa3d2130c25b1740867199d7b09e108e58ea7
Alexander S. Klenin <klenin@gmail.com>
2013-06-08 21:46:38 +1100

    Переписать задание Arch06
    * Исправить стиль и форматирование кода
    * Использовать хеш для проверки уникальности вариантов ответа
    * Использовать цикл вместо рекурсии для повторных попыток
    * Определять подпрограммы перед использованием

--- EGE/Gen/Arch06.pm
+++ EGE/Gen/Arch06.pm
@@ -12,35 +12,36 @@ use EGE::Random;
 use EGE::Asm::Processor;
 use EGE::Asm::AsmCodeGenerate;
 
-sub match_values {
-	my $self = shift;
-	my ($a, $b) = rnd->pick_n(2, ('a', 'b', 'c', 'd'));
-	my @regs8 = ($a.'l', $a.'h', $b.'l', $b.'h');
-	my @regs16 = ($a.'x', $b.'x');
-	cgen->{code} = [];
-	map cgen->add_command('mov', $_, $self->get_8b_val() * 256 + $self->get_8b_val()), @regs16;
-	cgen->add_command(rnd->pick('shl', 'shr', 'sal', 'sar', 'rol', 'ror'), rnd->pick(@regs8), 4);
-	cgen->add_command(rnd->pick('and', 'or', 'xor'), rnd->pick_n(2, @regs8));
-	cgen->add_command(rnd->pick('add', 'sub', 'adc', 'sbb'), rnd->pick_n(2, @regs16));
-	proc->run_code(cgen->{code});
-	my @vals = map proc->get_val($_), @regs8;
-	my $code_txt = cgen->get_code_txt('%04Xh');
-	$self->{text} = <<QUESTION
-Процессором был выполнен следующий код: $code_txt Установить соответствие между регистрами и полученными в них значениями
-QUESTION
-;
-	$self->{left_column} = \@regs8;
-	$self->formated_variants('%02Xh', @vals);
-	$self->{correct} = [0, 1, 2, 3];
-	my $unique_vals = 1;
-	for my $i (0..$#vals) {
-		$unique_vals = '' if ((grep $_ == $vals[$i], @vals) > 1);
-	}
-	$self->match_values if (!$unique_vals);
+sub random_byte {
+    my ($x, $y) = rnd->shuffle(rnd->pick(0, 15), rnd->in_range(1, 14));
+    $x * 16 + $y;
 }
 
-sub get_8b_val {
-	rnd->pick(0,1) ? rnd->pick(0,15) * 16 + rnd->in_range(1,14) : rnd->in_range(1,14) * 16 + rnd->pick(0,15);
+sub try_match_values {
+    my $self = shift;
+    my ($a, $b) = rnd->pick_n(2, qw(a b c d));
+    my @regs8 = ($a . 'l', $a . 'h', $b . 'l', $b . 'h');
+    my @regs16 = ($a . 'x', $b . 'x');
+    cgen->clear;
+    cgen->add_commands(
+        map([ 'mov', $_, random_byte() * 256 + random_byte() ], @regs16),
+        [ rnd->pick(qw(shl shr sal sar rol ror)), rnd->pick(@regs8), 4 ],
+        [ rnd->pick(qw(and or xor)), rnd->pick_n(2, @regs8) ],
+        [ rnd->pick(qw(add sub adc sbb)), rnd->shuffle(@regs16) ],
+    );
+    proc->run_code(cgen->{code});
+    my @vals = map proc->get_val($_), @regs8;
+    return if @vals != keys %{{ map { $_ => 1 } @vals }};
+
+    my $code_txt = cgen->get_code_txt('%04Xh');
+    $self->{text} =
+        "Процессором был выполнен следующий код: $code_txt " .
+        'Установить соответствие между регистрами и полученными в них значениями';
+    $self->{left_column} = \@regs8;
+    $self->formated_variants('%02Xh', @vals);
+    $self->{correct} = [ 0, 1, 2, 3 ];
 }
+
+sub match_values { 1 until $_[0]->try_match_values; }
 
 1;
