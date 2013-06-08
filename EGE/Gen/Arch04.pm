5a34356daafc3827265b91332b40c685a51c0504
Alexander S. Klenin <klenin@gmail.com>
2013-06-08 19:51:13 +1100

    Переписать задание Arch04::choose_commands
    * Исправить стиль и форматирование кода
    * Заменить ручное форматирование кода на вызов format_command
    * Использовать хеш для проверки уникальности вариантов ответа
    * Использовать цикл вместо рекурсии для повторных попыток

--- EGE/Gen/Arch04.pm
+++ EGE/Gen/Arch04.pm
@@ -12,55 +12,55 @@ use EGE::Random;
 use EGE::Asm::Processor;
 use EGE::Asm::AsmCodeGenerate;
 
-sub choose_commands {
-	my $self = shift;
-	my ($reg1, $reg2) = cgen->get_regs(8, 8);
-	my ($arg1, $arg2, $arg3, $cmd1, $cmd2, $cmd3);
-	map { $_ = rnd->in_range(1, 255) } ($arg1, $arg2, $arg3);
-	map { $_ = rnd->pick('add', 'sub') } ($cmd1, $cmd2, $cmd3);
-	$self->formated_variants('<code>%s</code>', "mov $reg1, $arg1", "$cmd1 $reg1, $arg2",
-		"mov $reg2, $reg1", "$cmd2 $reg1, $arg3", "$cmd3 $reg1, $reg2");
-	my $commands = [
-		['mov', $reg1, $arg1],
-		[$cmd1, $reg1, $arg2],
-		['mov', $reg2, $reg1],
-		[$cmd2, $reg1, $arg3],
-		[$cmd3, $reg1, $reg2]
-	];
-	my @res_arr;
-	my @correct_arr = (
-		[1, 0, 1, 1, 1],
-		[1, 1, 1, 0, 1],
-		[1, 0, 1, 0, 1]
-	);
-	my @incorrect_arr = (
-		[1, 1, 1, 1, 1],
-		[1, 0, 0, 0, 0],
-		[1, 1, 0, 0, 0],
-		[1, 1, 0, 1, 0]
-	);
-	push @res_arr, $self->get_res($commands, $_, $reg1) for (@correct_arr, @incorrect_arr);
-	my @ids = ();
-	for my $i (0..$#correct_arr) {
-		push @ids, $i if (! grep {$res_arr[$i] == $res_arr[$_] && $i != $_ } (0..$#res_arr));
-	}
-	$self->choose_commands(), return if ($#ids == -1);
-	my $id = rnd->pick(@ids);
-	$self->{text} = <<QUESTION
-Выделите команды, которые следует оставить в программе, чтобы после выполнения полученного кода в регистре $reg1 содержалось значение $res_arr[$id]
-QUESTION
-;
-	$self->{correct} = $correct_arr[$id];
+sub run_selected {
+    my ($self, $commands, $selected, $reg) = @_;
+    cgen->clear;
+    for my $i (0..$#$commands) {
+        cgen->add_commands($commands->[$i]) if $selected->[$i];
+    }
+    proc->run_code(cgen->{code})->get_val($reg);
 }
 
-sub get_res {
-	my ($self, $commands, $arr, $reg) = @_;
-	cgen->{code} = [];
-	for my $i (0..$#{$commands}) {
-		cgen->add_command(@{$commands->[$i]}) if ($arr->[$i]);
-	}
-	proc->run_code(cgen->{code});
-	proc->get_val($reg);
+sub try_choose_commands {
+    my $self = shift;
+    my ($reg1, $reg2) = cgen->get_regs(8, 8);
+    my ($arg1, $arg2, $arg3) = map rnd->in_range(1, 255), 1..3;
+    my ($cmd1, $cmd2, $cmd3) = map rnd->pick(qw(add sub)), 1..3;
+    my $commands = [
+        [ 'mov', $reg1, $arg1 ],
+        [ $cmd1, $reg1, $arg2 ],
+        [ 'mov', $reg2, $reg1 ],
+        [ $cmd2, $reg1, $arg3 ],
+        [ $cmd3, $reg1, $reg2 ],
+    ];
+    my @good = (
+        [ 1, 0, 1, 1, 1 ],
+        [ 1, 1, 1, 0, 1 ],
+        [ 1, 0, 1, 0, 1 ],
+    );
+    my @bad = (
+        [ 1, 1, 1, 1, 1 ],
+        [ 1, 0, 0, 0, 0 ],
+        [ 1, 1, 0, 0, 0 ],
+        [ 1, 1, 0, 1, 0 ],
+    );
+
+    my @res_good = map $self->run_selected($commands, $_, $reg1), @good;
+    my @res_bad = map $self->run_selected($commands, $_, $reg1), @bad;
+
+    my %res_idx = ();
+    ++$res_idx{$_} for @res_good, @res_bad;
+    return if grep { $res_idx{$_} > 1 } @res_good;
+
+    my $idx = rnd->in_range(0, $#good);
+    $self->{text} =
+        'Выделите команды, которые следует оставить в программе, ' .
+        "чтобы после выполнения полученного кода в регистре $reg1 " .
+        "содержалось значение $res_good[$idx]";
+    $self->formated_variants('<code>%s</code>', map cgen->format_command($_, '%d'), @$commands);
+    $self->{correct} = $good[$idx];
 }
+
+sub choose_commands { 1 until $_[0]->try_choose_commands; }
 
 1;
