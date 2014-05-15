16ea9b47c56439a4246ba321f60a2fca97613e5d
Alexander S. Klenin <klenin@gmail.com>
2014-05-15 17:33:36 +1100

    Переписать задание Db02
    * Исправить пробелы и форматирование
    * Упростить код
    * Выбирать поле "Товар" вместо несуществующего поля "id"

--- EGE/Gen/Db02.pm
+++ EGE/Gen/Db02.pm
@@ -9,51 +9,47 @@ use strict;
 use warnings;
 use utf8;
 
+use EGE::Html;
 use EGE::Random;
 use EGE::Prog;
 use EGE::Prog::Lang;
-use EGE::Html;
-use EGE::SQL::Table;
 use EGE::Russian::Product;
+use EGE::SQL::Table;
 use EGE::SQL::Queries;
 
 sub select_where {
     my ($self) = @_;
-    my @field = qw(Товар Количество Цена Затраты);
-    my $table_products = EGE::SQL::Table->new(['id', @field]); 
+    my @fields = qw(Товар Количество Цена Затраты);
+    my $products = EGE::SQL::Table->new([ 'id', @fields ]);
     my @candy = rnd->pick_n(9, @EGE::Russian::Product::candy);
-    my (@values); 
-    @values = map [ map rnd->in_range(10, 80) * 100, @candy ], 0..@field-2;
-    $table_products->insert_rows(@{EGE::Utils::transpose([ 0..$#candy ], \@candy, $values[0], $values[1], $values[2])});
-    my $cond = '';
-    my (@ans, $query, $select);
-    push @ans, 0 for @candy;
+    my @values = map [ map rnd->in_range(10, 80) * 100, @candy ], 0 .. @fields - 2;
+    $products->insert_rows(@{EGE::Utils::transpose([ 0..$#candy ], \@candy, @values)});
+    my (%ans, $query);
     while (1) {
-        my ($s1, $s2, $s3) = rnd->pick_n(3, 1 .. @field-1);
-        my $l = $table_products->random_val(@values);
-        my $r = $table_products->random_val(@values);
+        my ($f1, $f2, $f3) = rnd->shuffle(@fields[1 .. $#fields]);
+        my ($l, $r) = map $products->random_val(@values), 1..2;
         my $e = EGE::Prog::make_expr([
             rnd->pick('&&', '||'),
             [
                 rnd->pick('&&', '||'),
-                [ rnd->pick(ops::comp), $field[$s1], $l ], 
-                [ '>', $field[$s2], $field[$s1] ],
+                [ rnd->pick(ops::comp), $f1, $l ],
+                [ '>', $f2, $f1 ],
             ], 
-            [ rnd->pick(ops::comp), $field[$s3], $r ],
+            [ rnd->pick(ops::comp), $f3, $r ],
         ]);
-        $query = EGE::SQL::Select->new($table_products, 'products', [ 'id' ], $e);
-        $select = $query->run();
-        if ($select->count() -1 > 0&& $select->count() < $table_products->count()- 2) { 
-            $cond = html->cdata($e->to_lang_named('SQL'));
-            $ans[$$_[0]] = 1 for @{$select->{data}};
+        $query = EGE::SQL::Select->new($products, 'products', [ 'Товар' ], $e);
+        my ($selected) = $query->run();
+        if (1 < $selected->count() && $selected->count() < $products->count() - 2) {
+            $ans{$_->[0]} = 1 for @{$selected->{data}};
             last;
         }
     }
     $self->{text} =
-        "В таблице <tt>products</tt> представлен список товаров: \n".$table_products->select([@field])->table_html()."\n" .
-        "Какие товары в этой таблицы удовлетворяют запросу <tt>".$query->text_html()."</tt>?\n",
+        "В таблице <tt>products</tt> представлен список товаров: \n" .
+        $products->select([@fields])->table_html() . "\n" .
+        "Какие товары в этой таблицы удовлетворяют запросу " . $query->text_html() . "?\n",
     $self->variants(@candy);
-    $self->{correct} = \@ans;
+    $self->{correct} = [ map $ans{$_} ? 1 : 0, @candy ];
 }
 
 1;
