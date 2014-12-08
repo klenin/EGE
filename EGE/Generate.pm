ad293ddf93397155051af9c1a883743baf3f990c
pinkslot <pinkslot@mail.ru>
2014-12-09 04:37:03 +1100

    добавить импорт модулей новых заданий в Generate

--- EGE/Generate.pm
+++ EGE/Generate.pm
@@ -51,9 +51,11 @@ use EGE::Gen::B05;
 use EGE::Gen::B06;
 use EGE::Gen::B07;
 use EGE::Gen::B08;
+use EGE::Gen::B10;
 use EGE::Gen::B11;
 use EGE::Gen::B12;
 use EGE::Gen::B13;
+use EGE::Gen::B14;
 use EGE::Gen::B15;
 
 sub g {
@@ -96,9 +98,11 @@ sub all {[
     gg('B06', qw(solve)),
     gg('B07', qw(who_is_right)),
     gg('B08', qw(identify_letter find_calc_system)),
+    gg('B10', qw(trans_arch)),
     gg('B11', qw(ip_mask)),
     gg('B12', qw(search_query)),
     gg('B13', qw(plus_minus)),
+    gg('B14', qw(call_func)),    
     gg('B15', qw(logic_var_set)),
 ]}
 
