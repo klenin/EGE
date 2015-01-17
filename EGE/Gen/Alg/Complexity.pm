c1eca4d1fe6cc14bbb4e28ec9d10a33855f94451
Alexander S. Klenin <klenin@gmail.com>
2015-01-17 15:04:30 +1000

    Добавить задание Alg::Complexity::o_poly_cmp

--- EGE/Gen/Alg/Complexity.pm
+++ EGE/Gen/Alg/Complexity.pm
@@ -21,19 +21,29 @@ sub elem { # ax^y
     $a == 1 ? $p : [ '*', $a, $p ];
 }
 
+sub to_logic { EGE::Prog::make_expr($_[0])->to_lang_named('Logic') }
+sub big_o { "<i>O</i>($_[0])" }
+
 sub o_poly {
     my ($self) = @_;
-    my $n = "<i>n</i>";
     my $max_power = rnd->in_range(3, 5);
     my $max_coeff = rnd->in_range(2, 7);
     my @powers = sort { $b <=> $a } rnd->pick_n(4, 0 .. 6);
     my @coeffs = map rnd->in_range(1, 9), 1 .. 4;
-    my @elems = map elem($n, @$_), rnd->shuffle(@{EGE::Utils::transpose \@coeffs, \@powers});
-    my $func = EGE::Prog::make_expr(
-        List::Util::reduce { [ '+', $a, $b ] } @elems)->to_lang_named('Logic');
+    my @elems = map elem('n', @$_), rnd->shuffle(@{EGE::Utils::transpose \@coeffs, \@powers});
+    my $func = to_logic(List::Util::reduce { [ '+', $a, $b ] } @elems);
     $self->{text} ="Функция $func является";
-    $self->variants(map "<i>O</i>($_)", map EGE::Prog::make_expr($_)->to_lang_named('Logic'),
-        map(elem($n, 1, $_), grep $_ > 0, @powers), List::Util::max(@coeffs));
+    $self->variants(map big_o(to_logic($_)),
+        map(elem('n', 1, $_), grep $_ > 0, @powers), List::Util::max(@coeffs));
+}
+
+sub o_poly_cmp {
+    my ($self) = @_;
+    my $power = rnd->in_range(3, 6);
+    my ($func, @variants) = map big_o(to_logic(elem 'n', 1, $_)),
+        $power, $power + 1, $power - 1, [ '/', 1, $power ], 0, -$power;
+    $self->{text} ="Всякая функция, являющаяся $func, является также и";
+    $self->variants(@variants);
 }
 
 1;
--- gen.pl
+++ gen.pl
@@ -230,6 +230,7 @@ binmode STDOUT, ':utf8';
 #g2('Db08', 'nuncle') for 1..3;
 #g2('Db09', 'inner_join');
 #g3('Complexity', 'o_poly');
+#g3('Complexity', 'o_poly_cmp');
 
 #$questions = EGE::Generate::all;
 #$questions = EGE::AsmGenerate::all;
