5c3a4665d944037f6c02d430518bf633f0a1a655
pinkslot <pinkslot@mail.ru>
2015-02-21 14:13:40 +1100

    Изменить алгоритм из задания Alg::Complexity: ввести MAX_IF, MAX_ASSIGN, не генерировать неиспользуемые переменные, одинаковые IfThen

--- EGE/Gen/Alg/Complexity.pm
+++ EGE/Gen/Alg/Complexity.pm
@@ -51,9 +51,9 @@ use constant MAX_DEGREE => 3;
 
 sub pow {
     my ($var, $degree) = @_;
-    $degree == 0 and return rnd->const_value;
-    $degree == 1 and return $var;
-    [ '*', $var, pow($var, --$degree) ];
+    my $ret = $var;
+    $ret = [ '*', $var, $ret ] while --$degree;
+    $ret;
 }
 
 sub cycle_complexity
@@ -61,7 +61,7 @@ sub cycle_complexity
     my ($self) = @_;
     $self->{correct} = 0;
 
-    my $main_var = rnd->pick(qw(N M K));
+    my $main_var = rnd->pick(qw(x y z));
     my @vars = rnd->index_var(3);
     my @degrees = rnd->shuffle(1 .. MAX_DEGREE);
     @degrees = @degrees[ 1 .. 3 ];
@@ -96,46 +96,83 @@ sub new_var {
     $var;
 }
 
-sub rnd_expr { pow(rnd->pick(keys $_[0]), rnd->in_range(0, MAX_DEGREE)) }
-
-sub cycle {
-    my ($count, $is_it_iterator) = @_;
-    $_[0]-- or return final_expr($is_it_iterator);
-
-    if (List::Util::sum(values $is_it_iterator) > 1 and rnd->coin) {
-        my @if_vars = rnd->pick_n(2, map $is_it_iterator->{$_} ? $_ : (), keys $is_it_iterator);
-        return ('if', [ '==', @if_vars ], [ cycle($_[0], $is_it_iterator) ]);   
+sub rnd_pow {
+    my ($other_counts, $is_it_iterator, $assign, $create_new_var) = @_;
+    my $var = rnd->pick(keys $is_it_iterator);
+    if ($create_new_var && $other_counts->{assign} && rnd->coin) {
+        $other_counts->{assign}--;
+        my $right = rnd_pow($other_counts, $is_it_iterator, $assign, 0);
+        $var = new_var($is_it_iterator, 0);
+        push $assign, '=', $var, $right;
     }
-    
-    my @ret = ();
-    my $e = rnd_expr($is_it_iterator);
-    push @ret, ('=', new_var($is_it_iterator, 0), $e) while (rnd->coin); 
-
-    $e = rnd_expr($is_it_iterator);    
-    my $iter = new_var($is_it_iterator, 1);
-    push @ret, 'for', ($iter, 0, $e);
-    my $c = rnd->coin;
-    push @ret, $c? [cycle($_[0], $is_it_iterator)] : [ final_expr($is_it_iterator) ];
-    $is_it_iterator->{$iter} = 0;
-    
-    !$c and $_[0] and @ret = (@ret, cycle($_[0], $is_it_iterator));
-    @ret;
+    pow($var, rnd->in_range(1, MAX_DEGREE)); 
 }
 
-use constant MIN_COUNT => 4;
-use constant MAX_COUNT => 6;
+sub rnd_poly {
+    my $poly = rnd_pow(@_);
+    $poly = [ '+', $poly, rnd_pow(@_) ] while (rnd->coin);
+    $poly = [ '+', $poly, rnd->const_value ] if rnd->coin;
+    $poly;
+}
+
+sub make_cycle {
+    my ($for_count, $other_counts, $is_it_iterator) = @_;
+    if ($for_count) {
+
+        my $assign = [];
+        my $children = [];
+        my @head;
+        my $iter;
+
+        if (rnd->coin && List::Util::sum(values $is_it_iterator) > 1 && $other_counts->{if}--) {
+            my @if_vars = rnd->pick_n(2, map $is_it_iterator->{$_} ? $_ : (), keys $is_it_iterator); 
+            $is_it_iterator->{$iter = $if_vars[0]} = 0;
+            @head = ('if', [ '==', @if_vars ]);
+        }
+        else {
+        	$for_count--;
+            my $e = rnd_pow($other_counts, $is_it_iterator, $assign, 1);
+            $iter = new_var($is_it_iterator, 1);
+            @head = ('for', $iter, 0, $e);
+        }
+
+        my $next_for_count;
+        do {
+            $for_count -= $next_for_count = $for_count ? rnd->in_range(1, $for_count) : 0;
+            push $children, make_cycle($next_for_count, $other_counts, $is_it_iterator);
+        } while ($for_count);     
+        
+        $is_it_iterator->{$iter} ^= 1; 
+        return @$assign, @head, $children;
+
+    }
+    else {
+        return final_expr($is_it_iterator);        
+    }
+
+}
+
+use constant MIN_FOR => 4;
+use constant MAX_FOR => 6;
+use constant MAX_IF => 3;
+use constant MAX_ASSIGN => 5;
+
+
 
 sub complexity
 {
     my ($self) = @_;    
-    my $main_var = rnd->pick(qw(N M K));
-    my ($count, $block);
+    my $main_var = rnd->pick(qw(x y z));
     my @mistakes_names = qw(var_as_const ignore_if change_min); 
     $self->{correct} = 0;
 
     while(1) {
-        $count = rnd->in_range(MIN_COUNT, MAX_COUNT);
-        $block = EGE::Prog::make_block([ cycle($count, { $main_var => 0 }) ]);
+        my $cycle = [ make_cycle(
+            rnd->in_range(MIN_FOR, MAX_FOR), 
+            { if => MAX_IF, assign => MAX_ASSIGN }, 
+            { $main_var => 0 }) ];
+		
+        my $block = EGE::Prog::make_block($cycle);
         my @indexes = rnd->shuffle(1 .. 7);
         my @variants = $block->complexity({ $main_var => 1 });
         MISTAKE:
@@ -148,6 +185,10 @@ sub complexity
             $cur_variant == $_ and next MISTAKE for @variants;
             push @variants, $cur_variant;
             if (@variants == 4) {
+                if (rnd->coin && List::Util::max(@variants) == $variants[0]) { 
+                    pop @variants;
+                    next MISTAKE;
+                }
                 my $lt = EGE::LangTable::table($block, [ [ 'C', 'Basic' ], [ 'Pascal', 'Alg', 'Perl' ] ]);   
                 $self->{text} = "Определите асимптотическую сложность следующего алгоритма: $lt";
                 $self->variants(map big_o(to_logic([ '**', $main_var, $_ ])), @variants);
