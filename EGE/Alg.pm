01a030f1be50f360a3c5ddcf972e86055e7a9a34
pinkslot <pinkslot@mail.ru>
2015-03-05 02:05:43 +1100

    Вынести функции Alg::Complexity::to_logic big_o make_rnd_block в отдельный модуль EGE::Alg

---
+++ EGE/Alg.pm
@@ -0,0 +1,107 @@
+# Copyright © 2015 Alexander S. Klenin
+# Copyright © 2015 Anton Kim
+# Licensed under GPL version 2 or later.
+# http://github.com/klenin/EGE
+package EGE::Alg;
+
+use strict;
+use warnings;
+
+use EGE::Random;
+use base 'Exporter';
+our @EXPORT_OK = qw(to_logic big_o make_rnd_block);
+
+
+sub to_logic { EGE::Prog::make_expr($_[0])->to_lang_named('Logic') }
+sub big_o { "<i>O</i>($_[0])" }
+
+sub pow { $_[1] == 1 and $_[0] or [ '**', @_ ] }
+
+sub final_expr {
+    ('expr', [ 'print', @_ ]);
+}
+
+sub new_var {
+    my $var = rnd->index_var;
+    $var .= rnd->index_var while $_[0]->{$var};
+    $_[0]->{$var} = 1;
+    $var;
+}
+
+use constant MAX_DEGREE => 4;
+use constant P_RAND => 0.5;
+use constant P_IF => 0.5;
+use constant P_ASSIGN => 0.5;
+
+sub rnd_pow {
+    my ($vars, $use_iters, $other_counts, $assign) = @_;
+    my @all_vars = keys $vars->{all};
+    @all_vars = grep !$vars->{iterator}->{$_}, @all_vars if !$use_iters;
+    my $var = rnd->pick(@all_vars);
+    if ($assign && $other_counts->{assign} && rnd->coin(P_ASSIGN)) {
+        $other_counts->{assign}--;
+        my $right = rnd_pow($vars, 0, $other_counts);
+        $var = new_var($vars->{all});
+        push $assign, '=', $var, $right;
+    }
+    my $degree = rnd->coin(P_RAND) && $other_counts->{rand}-- > 0 ?
+        [ '()', 'rand', sort(map &rnd->in_range(1, MAX_DEGREE), 0 .. 1) ] :
+        rnd->in_range(1, MAX_DEGREE);
+    pow($var, $degree);
+    
+}
+
+sub rnd_poly {
+    my $poly = rnd_pow(@_);
+    $poly = [ '+', $poly, rnd_pow(@_) ] while (rnd->coin);
+    $poly = [ '+', $poly, rnd->const_value ] if rnd->coin;
+    $poly;
+}
+
+sub make_rnd_block {
+    my ($for_count, $other_counts, $vars) = @_;
+    if ($for_count) {
+        my $assign = [];
+        my $children = [];
+        my (@head, @cur_vars, $type);
+        if ($other_counts->{if} && rnd->coin(P_IF) && keys($vars->{iterator}) - keys($vars->{if}) >= 2) {
+            $type = $vars->{if};
+            my $cond;
+            my @not_used = grep(!$vars->{if}->{$_}, keys $vars->{iterator});
+            if (rnd->coin) {
+                @cur_vars = (rnd->shuffle(@not_used))[0 .. 1];
+                $cond = [ '==', @cur_vars ];
+            }
+            else {
+                push @cur_vars, rnd->pick(@not_used);
+                my $expr = rnd_pow($vars, 0, $other_counts, $assign);
+                $cond = rnd->coin ? [ '<=', @cur_vars, $expr ] : [ '>=', $expr, @cur_vars ];
+            }
+            @head = ('if', $cond);
+            $other_counts->{if}--;
+        }
+        else {
+            $type = $vars->{iterator};
+            my $expr = rnd_pow($vars, 1, $other_counts, $assign);
+            @cur_vars = new_var($vars->{all});
+            @head = ('for', @cur_vars, 0, $expr);
+            $other_counts->{counter} and push $children, '=', 'counter', [ '+', 'counter', 1 ];
+            $for_count--;
+        }
+
+        $type->{$_} = 1 for @cur_vars;
+        my $next_for_count;
+        do {
+            $for_count -= $next_for_count = $for_count ? rnd->in_range(1, $for_count) : 0;
+            push $children, make_rnd_block($next_for_count, $other_counts, $vars);
+        } while ($for_count);
+        delete $type->{$_} for @cur_vars; 
+        return @$assign, @head, $children;
+    }
+    else {
+        return final_expr(keys $vars->{iterator});
+    }
+
+}
+
+1;
--- EGE/Gen/Alg/Complexity.pm
+++ EGE/Gen/Alg/Complexity.pm
@@ -14,6 +14,7 @@ use List::Util;
 use EGE::Prog;
 use EGE::Random;
 use EGE::Utils;
+use EGE::Alg;
 
 sub elem { # ax^y
     my ($x, $a, $y) = @_;
@@ -22,9 +23,6 @@ sub elem { # ax^y
     $a == 1 ? $p : [ '*', $a, $p ];
 }
 
-sub to_logic { EGE::Prog::make_expr($_[0])->to_lang_named('Logic') }
-sub big_o { "<i>O</i>($_[0])" }
-
 sub o_poly {
     my ($self) = @_;
     my $max_power = rnd->in_range(3, 5);
@@ -32,28 +30,19 @@ sub o_poly {
     my @powers = sort { $b <=> $a } rnd->pick_n(4, 0 .. 6);
     my @coeffs = map rnd->in_range(1, 9), 1 .. 4;
     my @elems = map elem('n', @$_), rnd->shuffle(@{EGE::Utils::transpose \@coeffs, \@powers});
-    my $func = to_logic(List::Util::reduce { [ '+', $a, $b ] } @elems);
+    my $func = EGE::Alg::to_logic(List::Util::reduce { [ '+', $a, $b ] } @elems);
     $self->{text} ="Функция $func является";
-    $self->variants(map big_o(to_logic($_)),
+    $self->variants(map EGE::Alg::big_o(EGE::Alg::to_logic($_)),
         map(elem('n', 1, $_), grep $_ > 0, @powers), List::Util::max(@coeffs));
 }
 
 sub o_poly_cmp {
     my ($self) = @_;
     my $power = rnd->in_range(3, 6);
-    my ($func, @variants) = map big_o(to_logic(elem 'n', 1, $_)),
+    my ($func, @variants) = map EGE::Alg::big_o(EGE::Alg::to_logic(elem 'n', 1, $_)),
         $power, $power + 1, $power - 1, [ '/', 1, $power ], 0, -$power;
     $self->{text} ="Всякая функция, являющаяся $func, является также и";
     $self->variants(@variants);
-}
-
-use constant MAX_DEGREE => 2;
-
-sub pow {
-    my ($var, $degree) = @_;
-    my $ret = $var;
-    $ret = [ '*', $var, $ret ] while --$degree;
-    $ret;
 }
 
 sub cycle_complexity
@@ -61,16 +50,15 @@ sub cycle_complexity
     my ($self) = @_;
     $self->{correct} = 0;
 
-    my $main_var = rnd->pick(qw(x y z));
+    my $main_var = rnd->pick(qw(n m));
     my @vars = rnd->index_var(3);
-    my @degrees = rnd->shuffle(1 .. MAX_DEGREE);
-    @degrees = @degrees[ 1 .. 3 ];
+    my @degrees = rnd->shuffle(1 .. 3);
     my $cycles = [
-        'for', $vars[0], 0, pow($main_var, $degrees[0]), [
-            'for', $vars[1], 0, pow($main_var, $degrees[1]), [
+        'for', $vars[0], 0, EGE::Alg::pow($main_var, $degrees[0]), [
+            'for', $vars[1], 0, EGE::Alg::pow($main_var, $degrees[1]), [
                 '=', [ '[]', 'buf', $vars[1] ], $vars[1]
             ],
-            'for', $vars[2], 0, pow($main_var, $degrees[2]), [
+            'for', $vars[2], 0, EGE::Alg::pow($main_var, $degrees[2]), [
                 '=', [ '[]', 'buf', $vars[2] ], $vars[2]
             ]
         ]
@@ -78,101 +66,18 @@ sub cycle_complexity
     my $block = EGE::Prog::make_block($cycles);
     my $lt = EGE::LangTable::table($block, [ [ 'C', 'Basic' ], [ 'Pascal', 'Alg', 'Perl' ] ]);
     $self->{text} = "Определите асимптотическую сложность следующего алгоритма: $lt";
-    my @variants = ( $block->complexity($main_var),
+    my @variants = ( $block->complexity({ $main_var => 1 }),
         $degrees[0] + List::Util::min(@degrees[ 1 .. 2 ]),
         List::Util::sum(@degrees),
         $degrees[0] );
-    $self->variants(map big_o(to_logic([ '**', $main_var, $_ ])), @variants);
+    $self->variants(map EGE::Alg::big_o(EGE::Alg::to_logic([ '**', $main_var, $_ ])), @variants);
 }
 
-use constant COUNTER => 0;
-
-sub final_expr {
-    my $iter = $_[0];
-    ('expr', [ 'print', map $iter->{$_} ? $_ : (), keys $iter ]);
-}
-
-sub new_var {
-    my $var = rnd->index_var;
-    $var .= rnd->index_var while (defined $_[0]->{$var});
-    $_[0]->{$var} = $_[1];
-    $var;
-}
-
-sub rnd_pow {
-    my ($other_counts, $is_it_iterator, $assign, $create_new_var, $use_iter) = @_;
-    my @vars = keys $is_it_iterator;
-    !$use_iter and @vars = grep { !$is_it_iterator->{$_} } @vars;
-    my $var = rnd->pick(@vars);
-    if ($create_new_var && $other_counts->{assign} && rnd->coin) {
-        $other_counts->{assign}--;
-        my $right = rnd_pow($other_counts, $is_it_iterator, $assign, 0, $use_iter);
-        $var = new_var($is_it_iterator, 0);
-        push $assign, '=', $var, $right;
-    }
-    pow($var, rnd->in_range(1, MAX_DEGREE)); 
-}
-
-sub rnd_poly {
-    my $poly = rnd_pow(@_);
-    $poly = [ '+', $poly, rnd_pow(@_) ] while (rnd->coin);
-    $poly = [ '+', $poly, rnd->const_value ] if rnd->coin;
-    $poly;
-}
-
-use constant P_IF_EQ => 0.3;
-use constant P_IF_LESS => 0.3;
-
-sub make_cycle {
-    my ($for_count, $other_counts, $is_it_iterator) = @_;
-    if ($for_count) {
-
-        my $assign = [];
-        my $children = [];
-        my @head;
-        my $iter;
-        if (rnd->coin(P_IF_EQ) && List::Util::sum(values $is_it_iterator) > 1 && $other_counts->{if}) {
-            $other_counts->{if}--;
-            my @if_vars = rnd->pick_n(2, map $is_it_iterator->{$_} ? $_ : (), keys $is_it_iterator); 
-            $is_it_iterator->{$iter = $if_vars[0]} = 0;
-            @head = ('if', [ '==', @if_vars ]);
-        }
-        elsif (rnd->coin(P_IF_LESS) && List::Util::sum(values $is_it_iterator) > 0 && $other_counts->{if}) {
-            $other_counts->{if}--;
-            my $var = rnd->pick(grep { $is_it_iterator->{$_} } keys $is_it_iterator);
-            my $old_val = delete $is_it_iterator->{$var};
-            my $expr = rnd_pow($other_counts, $is_it_iterator, $assign, 0, 0);
-            $is_it_iterator->{$var} = $old_val;
-            @head = ('if', rnd->coin ? [ '<=', $var, $expr] : [ '>=', $expr, $var]);
-        }
-        else {
-            $for_count--;
-            my $e = rnd_pow($other_counts, $is_it_iterator, $assign, 1, 1);
-            $iter = new_var($is_it_iterator, 1);
-            @head = ('for', $iter, 0, $e);
-            push $children, '=', 'counter', [ '+', 'counter', 1 ];
-        }
-
-        my $next_for_count;
-        do {
-            $for_count -= $next_for_count = $for_count ? rnd->in_range(1, $for_count) : 0;
-            push $children, make_cycle($next_for_count, $other_counts, $is_it_iterator);
-        } while ($for_count);     
-        
-        $iter and $is_it_iterator->{$iter} ^= 1; 
-        return @$assign, @head, $children;
-
-    }
-    else {
-        return final_expr($is_it_iterator);        
-    }
-
-}
-
-use constant MIN_FOR => 2;
-use constant MAX_FOR => 3;
-use constant MAX_IF => 2;
-use constant MAX_ASSIGN => 2;
+use constant MAKE_COUNTER => 0;
+use constant MIN_FOR => 4;
+use constant MAX_FOR => 6;
+use constant MAX_IF => 4;
+use constant MAX_ASSIGN => 4;
 
 sub complexity
 {
@@ -182,10 +87,13 @@ sub complexity
     $self->{correct} = 0;
 
     while(1) {
-        my $cycle = [ '=', 'counter', 0, make_cycle(
+        my $vars = { all => { $main_var => 1 }, iterator => {}, if => {} };
+        my $cycle = [ EGE::Alg::make_rnd_block(
             rnd->in_range(MIN_FOR, MAX_FOR), 
-            { if => MAX_IF, assign => MAX_ASSIGN }, 
-            { $main_var => 0 }) ];
+            { if => MAX_IF, assign => MAX_ASSIGN, counter => MAKE_COUNTER },
+            $vars,
+            0) ];
+        MAKE_COUNTER and unshift $cycle, '=', 'counter', 0; 
         
         my $block = EGE::Prog::make_block($cycle);
         my @indexes = rnd->shuffle(1 .. 7);
@@ -206,20 +114,15 @@ sub complexity
                 }
                 my $lt = EGE::LangTable::table($block, [ [ 'C', 'Basic' ], [ 'Pascal', 'Alg', 'Perl' ] ]);   
                 $self->{text} = "Определите асимптотическую сложность следующего алгоритма: $lt";
-                if (COUNTER) {
+                if (MAKE_COUNTER) {
                     unshift($cycle, '=', $main_var, 10);
                     $self->{text} .= EGE::Prog::make_block($cycle)->run_val('counter');
                 }
-                $self->variants(map big_o(to_logic([ '**', $main_var, $_ ])), @variants);
+                $self->variants(map EGE::Alg::big_o(EGE::Alg::to_logic([ '**', $main_var, $_ ])), @variants);
                 return;
             }
-            
         } 
-
     }
 }
-
-
-
 
 1;
