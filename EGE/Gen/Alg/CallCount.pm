6122a9d894d7fad7f7961778dc669ccaee6f0244
pinkslot <pinkslot@mail.ru>
2015-03-07 07:13:48 +1100

    Небольшие исправления Alg::CallCount

--- EGE/Gen/Alg/CallCount.pm
+++ EGE/Gen/Alg/CallCount.pm
@@ -12,26 +12,26 @@ use List::Util;
 use EGE::Prog;
 use EGE::Random;
 
-sub div_up { $_[0] % $_[1] ? int($_[0] / $_[1]) + 1 : int($_[0] / $_[1]); }
-
 sub super_recursion {
     my ($self) = @_;
     my $n = 10**rnd->in_range(8, 12);
     my ($fst_part, $sec_part) = (int($n / rnd->in_range(2, 20)), int($n / rnd->in_range(2, 20) / 10)); 
     my ($div, $sub) = (rnd->in_range(2, 5), rnd->in_range(2, 5));
-    my $code = [ 'func', 'f', [ 'n' ], [
-        'if', [ '>=', 'n', $fst_part ], [
-            '=', 'f', [ '+', [ '()', 'f', [ '/', 'n', $div] ], 1 ]
+    my $code = [ 
+        'func', 'f', [ 'n' ], [
+            'if', [ '>=', 'n', $fst_part ], [
+                '=', 'f', [ '+', [ '()', 'f', [ '/', 'n', $div] ], 1 ]
+            ],
+            'if', [ '&&', [ '<', 'n', $fst_part ], [ '>=', 'n', $sec_part ] ], [
+                '=', 'f', ['+', [ '()', 'f', [ '-', 'n', $sub] ], 1 ]
+            ], 
+            'if', [ '<', 'n', $sec_part ], [
+                '=', 'f', 1 
+            ],
         ],
-        'if', [ '&&', [ '<', 'n', $fst_part ], [ '>=', 'n', $sec_part ] ], [
-            '=', 'f', ['+', [ '()', 'f', [ '-', 'n', $sub] ], 1 ]
-        ], 
-        'if', [ '<', 'n', $sec_part ], [
-            '=', 'f', 1 
-        ],
-    ],
-    '=', 'n', $n,
-    'expr', [ 'print', [ '()', 'f', 'n' ] ]
+        
+        '=', 'n', $n,
+        'expr', [ 'print', [ '()', 'f', 'n' ] ]
     ];
     my $lt = EGE::LangTable::table(EGE::Prog::make_block($code), [ [ 'C', 'Basic' ], [ 'Pascal', 'Alg', 'Perl' ] ]);
 
