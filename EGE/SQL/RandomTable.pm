e7dcfb119edf9b8f391f86e295b73520dc39d740
Alexander S. Klenin <klenin@gmail.com>
2015-04-30 14:01:18 +1000

    SQL::RandomTable: Исправить стиль и тексты вопросов

--- EGE/SQL/RandomTable.pm
+++ EGE/SQL/RandomTable.pm
@@ -12,28 +12,30 @@ use EGE::SQL::Utils;
 
 sub create_table {
     my %p = @_;
-    my @arr = ('EGE::SQL::Products', 'EGE::SQL::Jobs', 'EGE::SQL::ProductMonth',
-            'EGE::SQL::Cities', 'EGE::SQL::People', 'EGE::SQL::Subjects');
-    my $class = rnd->pick(grep ($_->get_column() >= $p{column} && grep (@$_ >= $p{row}, $_->get_rows_array()), @arr));
+    my $ok_table = sub {
+        (() = $_[0]->get_columns) >= $p{column} && grep scalar @$_ >= $p{row}, $_[0]->get_rows_array;
+    };
+    my $class = rnd->pick(grep $ok_table->($_), map "EGE::SQL::$_",
+        qw(Products Jobs ProductMonth Cities People Subjects));
     $class->make_table($p{column}, $p{row});
 }
 
 sub education_db {
     my ($self) = @_;
-    my $m = rnd->in_range(5,8);  my $i = 0;
-    my $lecturer = EGE::SQL::People->make_table(1, $m, 'lecturer');
+    my $m = rnd->in_range(5,8);
+    my $lecturer = EGE::SQL::People->make_table(1, $m, 'lecturers');
     my $student = EGE::SQL::People->make_table(1, $m, 'students');
     my $subject = EGE::SQL::Subjects->make_table(2, int($m / 2));
-    my @males = EGE::Russian::Names::different_males(2*$m);
-    $_->insert_column(name => 'Имя', array => [ @males[$m*$i..$m*++$i] ], index => 1) for ($student, $lecturer);
-    $_->insert_column(name => 'id', array => [1..$_->count()]) for ($subject, $student, $lecturer);
-    $lecturer->{text} = {$student->{name} => "У каких студентов преподает "};
-    $student->{text} = {$lecturer->{name} => "Какие преподаватели преподают у студента"};
-    $subject->{text} = {$student->{name} => "Кто изучает предмет" , $lecturer->{name} => "Кто ведет предмет"};
-    my @tables;
-    push @tables, EGE::SQL::Utils::related(${$lecturer->{fields}}[0], ${$subject->{fields}}[0] , @{$subject->column_array('id')});
-    push @tables, EGE::SQL::Utils::related(${$student->{fields}}[0], ${$subject->{fields}}[0], map rnd->pick(@{$subject->column_array('id')}), 1.. $m );
-    [ $lecturer, $student, $subject ], @tables;
+    my @males = EGE::Russian::Names::different_males(2 * $m);
+    my $i = 0;
+    $_->insert_column(name => 'Имя', array => [ @males[$m * $i .. $m * ++$i] ], index => 1) for $student, $lecturer;
+    $_->insert_column(name => 'id', array => [ 1 .. $_->count ]) for $subject, $student, $lecturer;
+    $lecturer->{text} = { $student->{name} => 'У каких студентов преподаёт' };
+    $student->{text} = { $lecturer->{name} => 'Какие преподаватели преподают у студента' };
+    $subject->{text} = { $student->{name} => 'Кто изучает предмет' , $lecturer->{name} => 'Кто преподаёт предмет' };
+    [ $lecturer, $student, $subject ],
+        EGE::SQL::Utils::related($lecturer->fields->[0], $subject->fields->[0], @{$subject->column_array('id')}),
+        EGE::SQL::Utils::related($student->fields->[0], $subject->fields->[0], map rnd->pick(@{$subject->column_array('id')}), 1 .. $m);
 }
 
 sub product_db {
@@ -44,60 +46,65 @@ sub product_db {
     my $cities = EGE::SQL::Cities->make_table(1, $m);
     my @males = EGE::Russian::Names::different_males($m);
     $buyers->insert_column(name => 'Имя', array => \@males , index => 1);
-    $_->insert_column(name => 'id', array => [1..$_->count()]) for ($cities, $product, $buyers);
-    $cities->{text} = {$product->{name} => "Какие товары покупают в городе "};
-    $product->{text} = {$cities->{name} => "В каком городе купили "};
-    $buyers->{text} = {$cities->{name} => "Где живет", $product->{name} => "Что купил"};
-    my @tables;
-    push @tables, EGE::SQL::Utils::related(${$cities->{fields}}[0], ${$buyers->{fields}}[0], @{$buyers->column_array('id')});
-    push @tables, EGE::SQL::Utils::related(${$product->{fields}}[0], ${$buyers->{fields}}[0], @{$buyers->column_array('id')});
-    [ $cities, $product, $buyers ] , @tables;
+    $_->insert_column(name => 'id', array => [ 1..$_->count ]) for $cities, $product, $buyers;
+    $cities->{text} = { $product->{name} => 'Какие товары покупают в городе' };
+    $product->{text} = { $cities->{name} => 'Где покупали' };
+    $buyers->{text} = { $cities->{name} => 'Где живет', $product->{name} => 'Что купил' };
+    [ $cities, $product, $buyers ],
+        EGE::SQL::Utils::related($cities->fields->[0], $buyers->{fields}->[0], @{$buyers->column_array('id')}),
+        EGE::SQL::Utils::related($product->fields->[0], $buyers->{fields}->[0], @{$buyers->column_array('id')});
 }
 
 package EGE::SQL::BaseTable;
 use EGE::Random;
+
 sub make_table {
     my ($self, $column_count, $row_count, $name) = @_;
-    my @columns = $self->get_column();
+    my @columns = $self->get_columns;
     my @fields = ($columns[0], rnd->pick_n($column_count - 1, @columns[1 .. $#columns]));
-    my @rows = rnd->pick_n($row_count, @{rnd->pick(grep @$_ >= $row_count, $self->get_rows_array())});
-    EGE::SQL::Utils::create_table(\@fields, \@rows, $name ? $name : $self->get_name());
+    my @row_sources = grep @$_ >= $row_count, $self->get_rows_array;
+    my @rows = rnd->pick_n($row_count, @{rnd->pick(@row_sources)});
+    EGE::SQL::Utils::create_table(\@fields, \@rows, $name || $self->get_name);
 }
 
 package EGE::SQL::Products;
 use base 'EGE::SQL::BaseTable';
 sub get_name { 'products' }
-sub get_column { my @product = ('Товар', 'Цена', 'Прибыль', 'Затраты', 'Выручка'); }
-sub get_rows_array { (\@EGE::Russian::Product::candy, \@EGE::Russian::Product::electronic, \@EGE::Russian::Product::pcs,
-    \@EGE::Russian::Product::printers, \@EGE::Russian::Product::laptops) }
+sub get_columns { qw(Товар Цена Прибыль Затраты Выручка); }
+sub get_rows_array { (
+    \@EGE::Russian::Product::candy,
+    \@EGE::Russian::Product::electronic,
+    \@EGE::Russian::Product::pcs,
+    \@EGE::Russian::Product::printers,
+    \@EGE::Russian::Product::laptops
+) }
 
 package EGE::SQL::Jobs;
 use base 'EGE::SQL::BaseTable';
 sub get_name { 'jobs' }
-sub get_column { my @jobs = ('Профессия', 'Зарплата'); }
-sub get_rows_array { (\@EGE::Russian::Jobs::list)}
+sub get_columns { ('Профессия', 'Зарплата'); }
+sub get_rows_array { (\@EGE::Russian::Jobs::list) }
 
 package EGE::SQL::ProductMonth;
 use base 'EGE::SQL::Products';
-sub get_column { my @products = ('Товар', @EGE::Russian::Time::month); }
+sub get_columns { ('Товар', @EGE::Russian::Time::month) }
 
 package EGE::SQL::Cities;
 use base 'EGE::SQL::BaseTable';
 sub get_name { 'cities' }
-sub get_column { my @cities = ('Город', 'Жители', 'Площадь'); }
+sub get_columns { qw(Город Жители Площадь) }
 sub get_rows_array { (\@EGE::Russian::City::city) }
 
 package EGE::SQL::People;
 use base 'EGE::SQL::BaseTable';
 sub get_name { 'people' }
-sub get_column { my @people = ('Фамилия', 'Зарплата'); }
+sub get_columns { qw(Фамилия Зарплата) }
 sub get_rows_array { (\@EGE::Russian::FamilyNames::list) }
 
 package EGE::SQL::Subjects;
 use base 'EGE::SQL::BaseTable';
 sub get_name { 'subject' }
-sub get_column { my @people = ('Предмет', 'Часы'); }
+sub get_columns { qw(Предмет Часы) }
 sub get_rows_array { (\@EGE::Russian::Subjects::list) }
-
 
 1;
