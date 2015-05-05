3515266706dacce95dd2fc85917a7cbf207da48d
pinkslot <pinkslot@github.com>
2015-05-05 12:20:16 +1000

    Изменить название sort_command->sort_line, исправления текста заданий

--- gen.pl
+++ gen.pl
@@ -250,7 +250,7 @@ binmode STDOUT, ':utf8';
 #g3('Tree', 'height');
 #g3('Graph', 'graph_seq');
 #g3('List', 'construct_command');
-#g3('Sorting', 'sort_command');
+#g3('Sorting', 'sort_line');
 #$questions = EGE::Generate::all;
 #$questions = EGE::AsmGenerate::all;
 #$questions = EGE::DatabaseGenerate::all;
