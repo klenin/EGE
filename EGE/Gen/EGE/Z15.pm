f26b2b408d543e6d33c4f4692619f212ef98adc0
Alexander S. Klenin <klenin@gmail.com>
2015-11-24 01:30:00 +1000

    Z15::city_roads: Исправить текст задания и стиль кода

--- EGE/Gen/EGE/Z15.pm
+++ EGE/Gen/EGE/Z15.pm
@@ -11,12 +11,13 @@ use warnings;
 use utf8;
 
 use EGE::Random;
+use EGE::Russian;
 use EGE::Html;
 use List::Util qw(min max);
 
 my @roads;
 my @city_path_count;
-my @city_id_to_char = ('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т');
+my @city_id_to_char = split //, 'АБВГДЕЖЗИКЛМНОПРСТ';
 
 my $last_city_id;
 my $look_back_size = 6;
@@ -27,12 +28,12 @@ my $max_last_city_id = 15;
 sub dfs {
     my $city_id = shift;
     return 1 if $city_id == $last_city_id;
-    return $city_path_count[$city_id] if ($city_path_count[$city_id]);
+    return $city_path_count[$city_id] if $city_path_count[$city_id];
     for (@{$roads[$city_id]}) {
-        $_ > $city_id or die "LOOP";
+        $_ > $city_id or die 'LOOP';
         $city_path_count[$city_id] += dfs($_);
     }
-    return $city_path_count[$city_id];
+    $city_path_count[$city_id];
 }
 
 sub city_roads {
@@ -41,34 +42,34 @@ sub city_roads {
     @roads = map [], 0..$last_city_id;
     @city_path_count = map 0, 0..$last_city_id;
 
-    for my $i(1..$last_city_id) {
-        for (rnd->pick_n(min(rnd->in_range(1, $max_inner_road_count), $i), max (0, $i-$look_back_size)..$i-1)) {
-            push @{$roads[$_]}, $i;
-        }
+    for my $i (1..$last_city_id) {
+        my $cnt = min(rnd->in_range(1, $max_inner_road_count), $i);
+        push @{$roads[$_]}, $i for rnd->pick_n($cnt, max(0, $i - $look_back_size) .. $i - 1);
     }
 
     my @top_row;
     my @middle_row;
     my @bottom_row;
-    for my $curr_city_id(0..$last_city_id) {
-        for my $next_city_id(@{$roads[$curr_city_id]}) {
+    for my $curr_city_id (0 .. $last_city_id) {
+        for my $next_city_id (@{$roads[$curr_city_id]}) {
             push @top_row, $city_id_to_char[$curr_city_id];
-            push @middle_row, "↓";
+            push @middle_row, '↓';
             push @bottom_row, $city_id_to_char[$next_city_id];
         }
     }
 
-    my $table = join '', map html->row("td", @{$_}), (\@top_row, \@middle_row, \@bottom_row); 
-
-    $self->{text} = 
-    html->p("В таблице представлена схема дорог соединяющих города ".
-    (join ', ', @city_id_to_char[0..$last_city_id]).". Двигаться по дорогам можно только 
-    из города указанном в верхней строке, в город указанный в нижней строке. 
-    Сколько существует различных дорог из города А в город $city_id_to_char[$last_city_id]?").
-    html->table($table, {html->style("margin-left" => "30px", "border-collapse" => "collapse"), border => "1px"});
+    $self->{text} = sprintf
+        '<p>В таблице представлена схема дорог, соединяющих города %s. ' .
+        'Двигаться по дорогам можно только из города, указанного в верхней строке, ' .
+        'в город, указанный в нижней строке. ' .
+        'Сколько существует различных дорог из города А в город %s?</p> %s',
+        EGE::Russian::join_comma_and(@city_id_to_char[0..$last_city_id]),
+        $city_id_to_char[$last_city_id],
+        html->table(
+            join('', map html->row('td', @$_), \@top_row, \@middle_row, \@bottom_row),
+            { html->style('margin-left' => '30px', 'border-collapse' => 'collapse'), border => '1px' });
 
     $self->{correct} = dfs(0);
-
     $self->accept_number;
 }
 
