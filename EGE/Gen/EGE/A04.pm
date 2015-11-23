57d0eca3c5854d59336cada39b64c68ad783ac86
lexeylk <lexey.lk@mail.ru>
2015-11-23 15:02:38 +1000

    Добавлен тест в задание А4
    Описание теста "count_zero_one": выбрать двоичное число, содержащее
    определенное количество нулей(единиц)

--- EGE/Gen/EGE/A04.pm
+++ EGE/Gen/EGE/A04.pm
@@ -10,6 +10,8 @@ use utf8;
 
 use EGE::Random;
 use EGE::Bin;
+use EGE::Bits;
+use EGE::NumText;
 
 sub sum {
     my ($self) = @_;
@@ -22,4 +24,33 @@ sub sum {
     $self->variants(map bin_hex_or_oct($_, rnd->in_range(0, 2)), $r, @errors);
 }
 
+sub generate_bin {
+    my($val, $size, $cur_count) = @_;
+    my $bin = EGE::Bits->new->set_size($size, $val);
+    if ($val == 0) { $bin->flip($size - 1) } else { $cur_count-- };
+    $bin->flip(rnd->pick_n($size - $cur_count - 1, 0..$size - 2));
+    return $bin->get_dec;
+}
+
+sub count_zero_one {
+    my ($self) = @_;
+    my $z_o = rnd->coin;
+    my $num_size = rnd->in_range(7, 11);
+    my $count = rnd->in_range(2, $num_size - 4);
+    my $type = $z_o == 1 ? num_one($count) : num_zero($count);
+
+    my @variants_ar;
+    for (my $i = 0; $i < 4; $i++){
+        @variants_ar[$i] = generate_bin($z_o, $num_size, $count + $i);
+    }
+
+    $self->{text} = <<QUESTION
+Для каждого из перечисленных ниже десятичных чисел построили
+двоичную запись. Укажите число, двоичная запись которого содержит
+ровно $type.
+QUESTION
+;
+    $self->variants(@variants_ar);
+    $self->{correct} = 0;
+}
 1;
