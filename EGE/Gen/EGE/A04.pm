0c7516390ca2019bd2d087488565f32e87b2c051
Alexander S. Klenin <klenin@gmail.com>
2015-11-23 22:38:48 +1000

    A04::count_zero_one: Переписать

--- EGE/Gen/EGE/A04.pm
+++ EGE/Gen/EGE/A04.pm
@@ -24,36 +24,28 @@ sub sum {
     $self->variants(map bin_hex_or_oct($_, rnd->in_range(0, 2)), $r, @errors);
 }
 
-sub num_zero { num_text($_[0], [ 'значащий нуль', 'значащих нуля', 'значащих нулей' ]) }
-sub num_one { num_text($_[0], [ 'единица', 'единицы', 'единиц' ]) }
-
-sub generate_bin {
-    my($val, $size, $cur_count) = @_;
-    my $bin = EGE::Bits->new->set_size($size, $val);
-    if ($val == 0) { $bin->flip($size - 1) } else { $cur_count-- };
-    $bin->flip(rnd->pick_n($size - $cur_count - 1, 0..$size - 2));
-    return $bin->get_dec;
+sub _generate_by_count {
+    my($val, $size, $count) = @_;
+    EGE::Bits->new->
+        set_bin([ 1, (1 - $val) x ($size - 1) ], 1)->
+        flip(rnd->pick_n($count - $val, 0 .. $size - 2))->
+        get_dec;
 }
 
 sub count_zero_one {
     my ($self) = @_;
-    my $z_o = rnd->coin;
-    my $num_size = rnd->in_range(7, 11);
+    my $val = rnd->coin;
+    my $num_size = rnd->in_range(7, 10);
     my $count = rnd->in_range(2, $num_size - 4);
-    my $type = $z_o == 1 ? num_one($count) : num_zero($count);
 
-    my @variants_ar;
-    for (my $i = 0; $i < 4; $i++){
-        @variants_ar[$i] = generate_bin($z_o, $num_size, $count + $i);
-    }
+    $self->variants(map _generate_by_count($val, $num_size, $count + $_), 0..2, -1);
 
-    $self->{text} = <<QUESTION
-Для каждого из перечисленных ниже десятичных чисел построили
-двоичную запись. Укажите число, двоичная запись которого содержит
-ровно $type.
-QUESTION
-;
-    $self->variants(@variants_ar);
-    $self->{correct} = 0;
+    $self->{text} = sprintf
+        'Для каждого из перечисленных ниже десятичных чисел построили двоичную запись. ' .
+        'Укажите число, двоичная запись которого содержит ровно %s.',
+        num_text($count, $val ?
+            [ 'единица', 'единицы', 'единиц' ] :
+            [ 'значащий нуль', 'значащих нуля', 'значащих нулей' ]);
 }
+
 1;
