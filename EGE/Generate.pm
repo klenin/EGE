3cb0db2839ebffecc17d8f7290cd3340252b3b24
Golovomoi <golovomoi@mail.ru>
2015-11-29 23:53:18 +1000

    добавить задание 6 : найти число следуя алгортиму

--- EGE/Generate.pm
+++ EGE/Generate.pm
@@ -57,6 +57,7 @@ use EGE::Gen::EGE::B12;
 use EGE::Gen::EGE::B13;
 use EGE::Gen::EGE::B14;
 use EGE::Gen::EGE::B15;
+use EGE::Gen::EGE::Z06;
 use EGE::Gen::EGE::Z15;
 use EGE::Gen::EGE::Z22;
 
@@ -110,6 +111,7 @@ sub all {[
     gg('B13', qw(plus_minus)),
     gg('B14', qw(find_func_min)),
     gg('B15', qw(logic_var_set)),
+    gg('Z15', qw(get_n)),
     gg('Z15', qw(city_roads)),
     gg('Z22', qw(calculator_find_prgm_count)),
 ]}
