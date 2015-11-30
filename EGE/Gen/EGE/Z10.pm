5d43518c21f58272aa02b89cb3f0fccf871d0850
Alexander S. Klenin <klenin@gmail.com>
2015-11-30 15:09:34 +1100

    Z10: Упростить код

--- EGE/Gen/EGE/Z10.pm
+++ EGE/Gen/EGE/Z10.pm
@@ -10,30 +10,23 @@ use utf8;
 
 use EGE::Random;
 
-sub words_count
-{
+sub words_count {
     my ($self) = @_;
-    my $n = rnd->in_range(3,6);
-    my $first_symb;
-    my $consonants = '';
-    my $vowels = '';
-    for my $i (1..rnd->in_range(1,3)){ $consonants = $consonants . rnd->russian_consonant_letter}; 
-    for my $i (1..rnd->in_range(1,3)){ $vowels = $vowels . rnd->russian_vowel_letter };
-    
-    foreach(($vowels,$consonants)) { $_=~ s/(.) (?: \1*)/$1/gx};
-    my @all_letters =split(//, $consonants . $vowels);
-    my $ans = @all_letters**$n;
-    
-    if (rnd->coin){
-        $first_symb = 'гласной';
-        $ans -= (length $consonants) * @all_letters**($n-1);
-    } else{
-        $first_symb = 'согласной';
-        $ans -= (length $vowels) * @all_letters**($n-1);
-    }
-    $self->{text} ="Сколь­ко слов длины $n, на­чи­на­ю­щих­ся с $first_symb буквы, можно со­ста­вить из букв: ". join(", ", @all_letters) .
-        ". Каж­дая буква может вхо­дить в слово не­сколь­ко раз.". 
-        "Слова не обя­за­тель­но долж­ны быть осмыс­лен­ны­ми сло­ва­ми рус­ско­го языка."; 
-    $self->{correct} = $ans;
+
+    my $n = rnd->in_range(4, 6);
+    my @consonants = rnd->pick_n(rnd->in_range(1,3), split //, 'БВГДЖЗКЛМНПРСТФХ');
+    my @vowels = rnd->pick_n($n - @consonants, split //, 'АЕИОУЫЭЮЯ');
+
+    my $r = rnd->coin ?
+      { first => 'гласной', num => @vowels * $n ** ($n-1) } :
+      { first => 'согласной', num => @consonants * $n ** ($n-1) };
+
+    $self->{text} =
+        "Сколь­ко слов длины $n, на­чи­на­ю­щих­ся с $r->{first} буквы, можно со­ста­вить из букв: " .
+        join(', ', rnd->shuffle(@consonants, @vowels)) .
+        '. Каж­дая буква может вхо­дить в слово не­сколь­ко раз. ' .
+        'Слова не обя­за­тель­но долж­ны быть осмыс­лен­ны­ми сло­ва­ми рус­ско­го языка.';
+    $self->{correct} = $r->{num};
 }
+
 1;
