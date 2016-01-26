9f5585eb7b5c1f9b83133689ce737337937e0e7d
Alexander S. Klenin <klenin@gmail.com>
2016-01-26 12:47:01 +1000

    Arch13: Исправить стиль
    * Удалить пробелы в конце строк
    * Удалить ненужный код
    * Исправить передачу параметров
    * Использовать стандартный список регистров

--- EGE/Gen/Arch/Arch13.pm
+++ EGE/Gen/Arch/Arch13.pm
@@ -1,5 +1,5 @@
-# Copyright © 2013 Alexander S. Klenin
-# Copyright © 2013 Nikita V. Dobrynin
+# Copyright © 2016 Alexander S. Klenin
+# Copyright © 2016 Nikita V. Dobrynin
 # Licensed under GPL version 2 or later.
 # http://github.com/klenin/EGE
 package EGE::Gen::Arch::Arch13;
@@ -14,18 +14,14 @@ use EGE::Asm::Processor;
 use EGE::Asm::AsmCodeGenerate;
 use EGE::Html;
 
-my @registers = ( 'eax', 'ebx', 'ecx', 'edx', 'esi', 'edi' );
 my $used_registers = 0;
 
 sub get_free_register {
-    die $used_registers if $used_registers >= @registers;
-    $registers[$used_registers++];
+    die $used_registers if $used_registers >= @EGE::Asm::Processor::registers;
+    $EGE::Asm::Processor::registers[$used_registers++];
 }
 
-sub free_last_register {
-    return if $used_registers <= 0;
-    $used_registers--;
-}
+sub free_last_register { $used_registers-- if $used_registers > 0 }
 
 my @operators = (
     {
@@ -61,8 +57,7 @@ sub gen_add_sub {
     my $node = shift;
     my @operands = @{$node->{operands}};
     my @operators = @{$node->{operators}};
-    my $first_complex_operand_pos = -1;
-    my $res_reg = 0; 
+    my $res_reg = 0;
     my $res_asm_list = [];
     for my $compl_op_pos (0 .. @operands - 1) {
         if (!exists $operands[$compl_op_pos]->{constant}) {
@@ -70,7 +65,7 @@ sub gen_add_sub {
             $res_reg = $t->{reg};
             $res_asm_list = $t->{asm_list};
             if ($compl_op_pos > 0 && $operators[$compl_op_pos - 1]->{eval_math} eq '-') {
-                push @$res_asm_list, ['neg', $res_reg];
+                push @$res_asm_list, [ 'neg', $res_reg ];
                 $operators[0] = $operators_by_eval_math{'+'};
             }
             splice @operands, $compl_op_pos, 1;
@@ -80,43 +75,42 @@ sub gen_add_sub {
     if (!$res_reg) {
         $res_reg = get_free_register();
         my $operand = shift @operands;
-        $res_asm_list = [['mov', $res_reg, $operand->{constant}]];
+        $res_asm_list = [[ 'mov', $res_reg, $operand->{constant} ]];
     }
     for my $op_pos (0 .. @operands - 1) {
         if (exists $operands[$op_pos]->{constant}) {
-            push @$res_asm_list, [$operators[$op_pos]->{asm}, $res_reg, $operands[$op_pos]->{constant}];
+            push @$res_asm_list, [ $operators[$op_pos]->{asm}, $res_reg, $operands[$op_pos]->{constant} ];
         } else {
             my $t = $operands[$op_pos]->{gen}->($operands[$op_pos]);
             push @$res_asm_list, @{$t->{asm_list}};
-            push @$res_asm_list, [$operators[$op_pos]->{asm}, $res_reg, $t->{reg}];
-            free_last_register();
+            push @$res_asm_list, [ $operators[$op_pos]->{asm}, $res_reg, $t->{reg} ];
+            free_last_register;
         }
     }
     { reg => $res_reg, asm_list => $res_asm_list };
 }
 
 sub append_operand {
-    (my $node, my $i, my $mutate_brackets, my $mutate_operators) = @_;
+    my ($node, $i, $mutate_brackets, $mutate_operators) = @_;
     my $operand = $node->{operands}->[$i];
     my $res_txt = gen_expression_text($operand, $mutate_brackets, $mutate_operators);
-    $res_txt = '(' . $res_txt . ')' if $node->{priority} < $operand->{priority} && (rnd->coin() || !$mutate_brackets);
-    $res_txt;
+    $node->{priority} < $operand->{priority} && (rnd->coin || !$mutate_brackets) ?
+        "($res_txt)" : $res_txt;
 }
 
 sub gen_expression_text {
-    (my $node, my $mutate_brackets, my $mutate_operators) = @_;
+    my ($node, $mutate_brackets, $mutate_operators) = @_;
     return $node->{constant} if exists $node->{constant};
     my $res_txt = '';
     for my $i (0 .. @{$node->{operands}} - 2) {
         my $curr_op = $node->{operators}->[$i];
-        $res_txt .= 
-            append_operand($node, $i, $mutate_brackets, $mutate_operators) . ' ' . 
-            (rnd->coin() && $mutate_operators ? 
-            $operators_by_id[rnd->in_range_except(0, @operators_by_id - 1, $curr_op->{id})]->{eval_math} : 
+        $res_txt .=
+            append_operand($node, $i, $mutate_brackets, $mutate_operators) . ' ' .
+            (rnd->coin && $mutate_operators ?
+            $operators_by_id[rnd->in_range_except(0, @operators_by_id - 1, $curr_op->{id})]->{eval_math} :
             $curr_op->{eval_math}) . ' ';
     }
-    $res_txt .= append_operand($node, @{$node->{operands}} - 1, $mutate_brackets, $mutate_operators);
-    $res_txt;
+    $res_txt . append_operand($node, @{$node->{operands}} - 1, $mutate_brackets, $mutate_operators);
 }
 
 sub get_random_operand {
@@ -124,8 +118,8 @@ sub get_random_operand {
 }
 
 sub make_expression {
-    (my $n, my $op_priority) = @_;
-    return get_random_operand() if ($n == 1);
+    my ($n, $op_priority) = @_;
+    return get_random_operand() if $n == 1;
     my $operands = [];
     my $operators = [];
     while ($n > 0) {
@@ -153,17 +147,18 @@ sub convert_eval_to_html_format {
 }
 
 sub check_for_repeats {
-    (my $a, my @arr) = @_;
+    my ($a, @arr) = @_;
+    my $v = eval $a;
     for my $i (@arr) {
-        return 1 if eval $a == eval $i;
+        return 1 if $v == eval $i;
     }
-    return 0;
+    0;
 }
 
 sub gen_variants {
-    (my $n, my $node) = @_;
-    my @res = (gen_expression_text($node, 0, 0));
-    my @mutate_args = ([0, 1], [1, 1]);
+    my ($n, $node) = @_;
+    my @res = gen_expression_text($node, 0, 0);
+    my @mutate_args = ([ 0, 1 ], [ 1, 1 ]);
     for my $i (1 .. $n - 1) {
         my $mutate_arg = rnd->pick(@mutate_args);
         my $curr_variant;
@@ -179,7 +174,6 @@ sub gen_variants {
 }
 
 sub print_hint {
-    my $res = '';
     my $r = html->row('th', qw(Приоритет Оператор Описание));
     for my $priority (0 .. @operators - 1) {
         $r .= join '', map html->row('td', ($priority + 1, $_->{html_math}, $_->{hint})),
@@ -203,13 +197,13 @@ sub expression_calc {
     my $self = shift;
     $used_registers = 0;
     init_operators();
-    
+
     my $node = make_expression(rnd->in_range(6, 12), 1);
-    
+
     my $t = $node->{gen}->($node);
     my @asm_list = @{$t->{asm_list}};
     cgen->set_commands(@asm_list);
-    
+
     $self->{text} = sprintf
         'Укажите формулу, которую будет вычислять следующий код: ' .
         '%s %s', 
