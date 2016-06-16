41b4b615825e2e1e60d90506658553bae2e9db70
Alexander S. Klenin <klenin@gmail.com>
2016-06-17 06:04:10 +1000

    Db02::select_where: Упростить код

--- EGE/Gen/Db/Db02.pm
+++ EGE/Gen/Db/Db02.pm
@@ -13,31 +13,26 @@ use EGE::Html;
 use EGE::Random;
 use EGE::Prog;
 use EGE::Prog::Lang;
-use EGE::Russian::Product;
 use EGE::SQL::Table;
 use EGE::SQL::Queries;
 
 sub select_where {
     my ($self) = @_;
-    my $rt = EGE::SQL::RandomTable->new(column => 4, row => 8);
+    my $rt = EGE::SQL::RandomTable->new(column => 4, row => rnd->in_range(7, 9));
     my $rt_class = $rt->pick;
-    my $products = $rt->make;
-    my $name_fld = $products->fields->[0];
-    my ($selected, $query);
-    do {
-        my $cond = EGE::SQL::Utils::generate_nontrivial_cond($products, \&EGE::SQL::Utils::expr_2);
-        $query = EGE::SQL::Select->new($products, [ $name_fld ], $cond);
-        $selected = $query->run;
-    } until $selected->count > 1;
-    my %ans;
-    $ans{$_->[0]} = 1 for @{$selected->{data}};
+    my $table = $rt->make;
+    my $name_fld = $table->fields->[0];
+    my $query = EGE::SQL::Select->new(
+        $table, [ $name_fld ],
+        EGE::SQL::Utils::generate_nontrivial_cond($table, \&EGE::SQL::Utils::expr_2));
+    my $selected = $query->run->column_hash($name_fld);
     $self->{text} = sprintf
         "Имеется таблица <tt>%s</tt>:\n%s\n" .
         'Какие %s в этой таблице удовлетворяют запросу %s?',
-        $products->name, $products->table_html,
+        $table->name, $table->table_html,
         $rt_class->get_text_name->{nominative}, $query->text_html_tt;
-    $self->variants(my @v = @{$products->column_array($name_fld)});
-    $self->{correct} = [ map $ans{$_} ? 1 : 0, @v ];
+    $self->variants(my @v = @{$table->column_array($name_fld)});
+    $self->{correct} = [ map $selected->{$_} ? 1 : 0, @v ];
 }
 
 1;
