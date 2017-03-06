4d21ae4f119f6061fd3f7d1c254ef4fea30b28c3
EPMAK97 <donskoi.ia@students.dvfu.ru>
2017-03-07 00:16:19 +1000

    Добавить варианты заданий B10

--- EGE/Gen/EGE/B10.pm
+++ EGE/Gen/EGE/B10.pm
@@ -10,6 +10,8 @@ use utf8;
 
 use EGE::NumText;
 use EGE::Random;
+use EGE::Russian;
+use EGE::Russian;
 
 sub trans_rate {
     my ($self) = @_;
@@ -91,32 +93,82 @@ sub trans_latency {
     $self->{correct} = $latency;
 }
 
+
+sub genitive { # родительный падеж
+    my $name = shift;
+    if ($name =~/й$/) { $name =~ s/й$/я/ }
+    elsif ($name =~ /ь$/) { $name =~ s/ь$/я/ }
+    elsif ($name =~ /ка$/) { $name =~ s/а$/и/ }
+    elsif ($name =~ /га$/) { $name =~ s/га$/ги/ }
+    elsif ($name =~ /eа$/) { $name =~ s/eа$/eи/ }
+    elsif ($name =~ /а$/) { $name =~ s/а$/ы/ }
+    elsif ($name =~ /я$/) { $name =~ s/я$/и/ }
+    elsif ($name =~ /eв$/) { $name =~ s/ев$/ьва/ }
+    else { $name .= 'а' };
+    $name;
+}
+
+sub ablative { # творительный падеж
+    my $name = shift;
+    if ($name =~ /й$/) { $name =~ s/й$/ем/ }
+    elsif ($name =~ /ь$/) { $name =~ s/ь$/ем/ }
+    elsif ($name =~ /eа$/) { $name =~ s/eа$/eй/ }
+    elsif ($name =~ /а$/) { $name =~ s/а$/ой/ }
+    elsif ($name =~ /я$/) { $name =~ s/я$/ей/ }
+    elsif ($name =~ /eв$/) { $name =~ s/ев$/ьвом/ }
+    else { $name .= 'ом' };
+    $name;
+}
+
+sub dative { # дательный падеж
+    my $name = shift;
+    if ($name =~ /й$/) { $name =~ s/й$/ю/ }
+    elsif ($name =~ /ь$/) { $name =~ s/ь$/ю/ }
+    elsif ($name =~ /а$/) { $name =~ s/а$/е/ }
+    elsif ($name =~ /я$/) { $name =~ s/я$/и/ }
+    elsif ($name =~ /eв$/) { $name =~ s/ев$/ьву/ }
+    else { $name .= 'у' };
+    $name;
+}
+
 sub min_period_of_time {
     my ($self) = @_;
 
     my $high_speed = rnd->in_range(17, 23);
     my $slow_speed = rnd->in_range(12, 15);
-    my $required_data  = rnd->in_range(6, 12);
-    my $full_data  = 2 ** rnd->in_range($high_speed - 13, 10);
+    my $required_data = rnd->in_range(6, 12);
+    my $full_data = 2 ** rnd->in_range($high_speed - 13, 10);
 
-    my @female_names = qw(Кат Тан Даш и ей);
-    my @male_names = qw(Серг Андр Алекс ея ей ею еем);
-    my $f_name = $female_names[rnd->in_range(0,2)];
-    my $m_name = $male_names[rnd->in_range(0,2)];
+    my $male_or_female  = rnd->coin;
+    my $female_name = $EGE::Russian::Names::female[rnd->in_range(0, 100)];
+    my $male_name = $EGE::Russian::Names::male[rnd->in_range(0, 100)];
+    my $name_first = $male_or_female ? $female_name : $male_name;
+    my $name_second = $male_or_female ? $male_name : $female_name;
+    my $word1 = $male_or_female ? 'договорился' : 'договорилась';
+    my $word2 = $male_or_female ? 'она' : 'он';
+
+    my %data = (
+        genitive_first  => genitive($name_first),
+        ablative_first  => ablative($name_first),
+        dative_first    => dative($name_first),
+        genitive_second => genitive($name_second),
+        ablative_second => ablative($name_second),
+        dative_second   => dative($name_second),
+    );
 
     $self->{text} =
-        "У $f_name$female_names[3] есть доступ к сети Интернет по высокоскоростному одностороннему радиоканалу,
+        "У $data{genitive_first} есть доступ к сети Интернет по высокоскоростному одностороннему радиоканалу,
         обеспечивающему скорость получения информации 2<sup>$high_speed</sup> бит в секунду.
-        У $m_name$male_names[3] нет скоростного доступа в Интернет, но есть возможность получать информацию
-        от $f_name$female_names[3] по телефонному каналу со средней скоростью 2<sup>$slow_speed</sup> бит в секунду.
-        $m_name$male_names[4] договорился с $f_name$female_names[4], что она скачает для него данные объемом $required_data
-         Мбайт по высокоскоростному каналу и ретранслирует их $m_name$male_names[5] по низкоскоростному каналу.<br/>
-        <br/>Компьютер $f_name$female_names[3] может начать ретрансляцию данных не раньше, чем им будут получены
+        У $data{genitive_second} нет скоростного доступа в Интернет, но есть возможность получать информацию
+        от $data{genitive_first} по телефонному каналу со средней скоростью 2<sup>$slow_speed</sup> бит в секунду.
+        $name_second $word1 с $data{ablative_first}, что $word2 скачает для него данные объемом $required_data
+         Мбайт по высокоскоростному каналу и ретранслирует их $data{dative_second} по низкоскоростному каналу.<br/>
+        <br/>Компьютер $data{genitive_first} может начать ретрансляцию данных не раньше, чем им будут получены
         первые $full_data Кбайт этих данных. Каков минимально возможный промежуток времени
-        (в секундах) с момента начала скачивания $f_name$female_names[4] данных до полного их получения
-        $m_name$male_names[6]?<br/><br/>В ответе укажите только число, слово «секунд» или букву «с» добавлять не нужно.";
+        (в секундах) с момента начала скачивания $data{ablative_first} данных до полного их получения
+        $data{ablative_second}?<br/><br/>В ответе укажите только число, слово «секунд» или букву «с» добавлять не нужно.";
 
-    $self->{correct} =  2 ** (23 - $slow_speed) * $required_data  + $full_data * 2 ** (13 - $high_speed);
+    $self->{correct} = 2 ** (23 - $slow_speed) * $required_data  + $full_data * 2 ** (13 - $high_speed);
     $self->accept_number;
 }
 
