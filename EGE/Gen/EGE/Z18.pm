22a03398db05ac4a48e252c39ebe2cac22f3e500
dddeeemmmooonnn <dddeeemmmooonnntrue@gmail.com>
2017-03-16 17:01:25 +1000

    Правки
    Изменены копирайты
    Добавлена проверка на неверный ответ
    Добавлен курсив к переменным в описании и unicode ∨

--- EGE/Gen/EGE/Z18.pm
+++ EGE/Gen/EGE/Z18.pm
@@ -1,5 +1,4 @@
-# Copyright © 2010-2012 Alexander S. Klenin
-# Copyright © 2012 V. Kevroletin
+# Copyright © 2010-2017 Alexander S. Klenin
 # Licensed under GPL version 2 or later.
 # http://github.com/klenin/EGE
 package EGE::Gen::EGE::Z18;
@@ -15,15 +14,18 @@ sub bitwise_conjunction {
     my ($self) = @_;
     my $p1 = rnd->in_range(1, 255);
     my $p2 = rnd->in_range_except(1, 255, $p1);
-    $self->{correct} = $p1 & $p2 ^ $p1;
+    my $ans = $p1 & $p2 ^ $p1;
+    $self->{correct} = $ans;
+    my $x = rnd->in_range(1, 255);
+    die 'Z18: Wrong Genereted Answer' unless ($x & $p1) == 0 || ($x & $p2) != 0 || ($x & $ans) != 0;
     $self->accept_number();
     $self->{text} = <<QUESTION
-        Обозначим через m&amp;n поразрядную конъюнкцию неотрицательных целых
-        чисел m и n. Так, например, 14&amp;5 = 1110<sub>2</sub>&amp;0101<sub>2</sub> = 0100<sub>2</sub> = 4.
-        Для какого наименьшего неотрицательного целого числа А формула
-        <p>x&amp;$p1 = 0 \/ (x&amp;$p2 = 0 → x&amp;А ≠ 0)</p>
+        Обозначим через <i>m</i>&amp;<i>n</i> поразрядную конъюнкцию неотрицательных целых
+        чисел <i>m</i> и <i>n</i>. Так, например, 14&amp;5 = 1110<sub>2</sub>&amp;0101<sub>2</sub> = 0100<sub>2</sub> = 4.
+        Для какого наименьшего неотрицательного целого числа <i>А</i> формула
+        <blockquote><i>x</i>&amp;$p1 = 0 ∨ (<i>x</i>&amp;$p2 = 0 → <i>x</i>&amp;<i>А</i> ≠ 0)</blockquote>
         тождественно истинна (т.е. принимает значение 1 при любом
-        неотрицательном целом значении переменной х)?
+        неотрицательном целом значении переменной <i>х</i>)?
 QUESTION
 }
 
