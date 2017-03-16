84609b9963c8baf7a5f622e9a65409cc70695f10
Alexander S. Klenin <klenin@gmail.com>
2017-03-16 17:45:27 +1000

    Z18::bitwise_conjunction: Добавить пробелы в текст, мелкие правки стиля

--- EGE/Gen/EGE/Z18.pm
+++ EGE/Gen/EGE/Z18.pm
@@ -15,18 +15,20 @@ sub bitwise_conjunction {
     my $p1 = rnd->in_range(1, 255);
     my $p2 = rnd->in_range_except(1, 255, $p1);
     my $ans = $p1 & $p2 ^ $p1;
-    $self->{correct} = $ans;
     my $x = rnd->in_range(1, 255);
-    die 'Z18: Wrong Genereted Answer' unless ($x & $p1) == 0 || ($x & $p2) != 0 || ($x & $ans) != 0;
-    $self->accept_number();
+    ($x & $p1) == 0 || ($x & $p2) != 0 || ($x & $ans) != 0 or die;
+    my $and = '&nbsp;&amp;&nbsp;';
     $self->{text} = <<QUESTION
-        Обозначим через <i>m</i>&amp;<i>n</i> поразрядную конъюнкцию неотрицательных целых
-        чисел <i>m</i> и <i>n</i>. Так, например, 14&amp;5 = 1110<sub>2</sub>&amp;0101<sub>2</sub> = 0100<sub>2</sub> = 4.
+        Обозначим через <i>m</i>$and<i>n</i> поразрядную конъюнкцию неотрицательных целых чисел <i>m</i> и <i>n</i>.
+        Так, например, 14${and}5 = 1110<sub>2</sub>${and}0101<sub>2</sub> = 0100<sub>2</sub> = 4.
         Для какого наименьшего неотрицательного целого числа <i>А</i> формула
-        <blockquote><i>x</i>&amp;$p1 = 0 ∨ (<i>x</i>&amp;$p2 = 0 → <i>x</i>&amp;<i>А</i> ≠ 0)</blockquote>
+        <blockquote><i>x</i>$and$p1 = 0 ∨ (<i>x</i>$and$p2 = 0 → <i>x</i>$and<i>А</i> ≠ 0)</blockquote>
         тождественно истинна (т.е. принимает значение 1 при любом
         неотрицательном целом значении переменной <i>х</i>)?
 QUESTION
+;
+    $self->{correct} = $ans;
+    $self->accept_number;
 }
 
 1;
